8319c3c4f8bdd7e77da6918cd953362016788191
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-07-16 12:57:09 +0000

    added profiling to the illiad queries, added some more indexes to the illiad schema to make it faster

--- grails-app/services/metridoc/Illiad/IlliadService.groovy
+++ grails-app/services/metridoc/Illiad/IlliadService.groovy
@@ -1,17 +1,17 @@
 package metridoc.Illiad
 
-import javax.sql.DataSource
-import org.codehaus.groovy.grails.commons.ConfigurationHolder
 import groovy.sql.Sql
 import metridoc.utils.DateUtil
 
+import javax.sql.DataSource
+
 class IlliadService {
 
-	private static int GROUP_ID_OTHER = -2;
-	private static int GROUP_ID_TOTAL = -1;
+    private static int GROUP_ID_OTHER = -2;
+    private static int GROUP_ID_TOTAL = -1;
 
     static transactional = true
-	DataSource dataSource_illiad
+    DataSource dataSource_illiad
     def grailsApplication
 
     def getQueries() {
@@ -20,20 +20,20 @@ class IlliadService {
 
 
     def getBasicStatsData(fiscalYear) {
-		Sql sql = new Sql(dataSource_illiad);
-		def result = ['books':[:], 'articles':[:]];
-		def reportFiscalYear = fiscalYear != null?fiscalYear: DateUtil.getCurrentFiscalYear();
+        Sql sql = new Sql(dataSource_illiad);
+        def result = ['books': [:], 'articles': [:]];
+        def reportFiscalYear = fiscalYear != null ? fiscalYear : DateUtil.getCurrentFiscalYear();
 
-		Date fiscalYearStart = DateUtil.getFiscalYearStartDate(reportFiscalYear)
-		Date fiscalYearEnd = DateUtil.getFiscalYearEndDate(reportFiscalYear)
+        Date fiscalYearStart = DateUtil.getFiscalYearStartDate(reportFiscalYear)
+        Date fiscalYearEnd = DateUtil.getFiscalYearEndDate(reportFiscalYear)
         result.books.borrowing = loadSectionData(sql, true, true, fiscalYearStart, fiscalYearEnd);
-		result.books.lending = loadSectionData(sql, true, false, fiscalYearStart, fiscalYearEnd);
-		result.articles.borrowing = loadSectionData(sql, false, true, fiscalYearStart, fiscalYearEnd);
-		result.articles.lending = loadSectionData(sql, false, false, fiscalYearStart, fiscalYearEnd);
-		return result;
+        result.books.lending = loadSectionData(sql, true, false, fiscalYearStart, fiscalYearEnd);
+        result.articles.borrowing = loadSectionData(sql, false, true, fiscalYearStart, fiscalYearEnd);
+        result.articles.lending = loadSectionData(sql, false, false, fiscalYearStart, fiscalYearEnd);
+        return result;
     }
 
-	def loadSectionData(sql, isBooks, isBorrowing, startDate, endDate){
+    def loadSectionData(sql, isBooks, isBorrowing, startDate, endDate) {
 
         log.info("loading sectional data with isBooks: $isBooks, isBorrowing: $isBorrowing, startDate: $startDate, endDate: $endDate")
 
@@ -46,85 +46,97 @@ class IlliadService {
         def turnaroundPerGroupQuery = pickQuery(queries.transactionTurnaroundsBorrowing, queries.transactionTurnaroundsLending)
 
 
-		def processType = isBorrowing ? 'Borrowing' : 'Lending';
-		def requestType = isBooks ? 'Loan' : 'Article';
+        def processType = isBorrowing ? 'Borrowing' : 'Lending';
+        def requestType = isBooks ? 'Loan' : 'Article';
 
-		def result = [:];
+        def result = [:];
 
-		def sqlParams = [requestType, startDate, endDate];
-		String queryFilled = getAdjustedQuery(genQuery,
-			['add_condition': ' and transaction_status=\'Request Finished\'']);
+        def sqlParams = [requestType, startDate, endDate];
+        String queryFilled = getAdjustedQuery(genQuery,
+            ['add_condition': ' and transaction_status=\'Request Finished\'']);
 
-		String queryExhausted = getAdjustedQuery(genQuery,
-			['add_condition': ' and not (transaction_status<=>\'Request Finished\')']);
+        String queryExhausted = getAdjustedQuery(genQuery,
+            ['add_condition': ' and not (transaction_status<=>\'Request Finished\')']);
 
-		log.info("Running query for filledQueries (borrowing=${isBorrowing}, book=${isBooks}): " + queryFilled + " params="+sqlParams)
-		sql.eachRow(queryFilled, sqlParams, {
-			int groupId = it.getAt(0) != null?it.getAt(0):GROUP_ID_TOTAL;
-			def groupData = getGroupDataMap(groupId, result)
-			groupData.filledRequests = it.transNum
-			groupData.sumFees = it.sumFees
-			//setTurnarounds(isBorrowing, groupData, it)
-		})
+        profile("Running query for filledQueries (borrowing=${isBorrowing}, book=${isBooks}): " + queryFilled + " params=" + sqlParams) {
+            sql.eachRow(queryFilled, sqlParams, {
+                int groupId = it.getAt(0) != null ? it.getAt(0) : GROUP_ID_TOTAL;
+                def groupData = getGroupDataMap(groupId, result)
+                groupData.filledRequests = it.transNum
+                groupData.sumFees = it.sumFees
+                //setTurnarounds(isBorrowing, groupData, it)
+            })
+        }
 
-		log.debug("Running query for turnaroundPerGroupQuery (borrowing=${isBorrowing}, book=#{isBook}): " + turnaroundPerGroupQuery + " params="+sqlParams)
-		sql.eachRow(turnaroundPerGroupQuery, sqlParams, {
-			int groupId = it.getAt(0);
-			def groupData = getGroupDataMap(groupId, result)
-			setTurnarounds(isBorrowing, groupData, it)
-		})
+        profile("Running query for turnaroundPerGroupQuery (borrowing=${isBorrowing}, book=#{isBook}): " + turnaroundPerGroupQuery + " params=" + sqlParams) {
+            sql.eachRow(turnaroundPerGroupQuery, sqlParams, {
+                int groupId = it.getAt(0);
+                def groupData = getGroupDataMap(groupId, result)
+                setTurnarounds(isBorrowing, groupData, it)
+            })
+        }
 
-		/* Set corrected turnarounds for total row(rollup double counts
-		   because transaction can belong to more then one group) */
-		log.debug("Running query for turnaroundQuery (total) (borrowing=${isBorrowing}, book=#{isBook}): " + turnaroundQuery + " params="+sqlParams)
-		def totalGroupTurnarounds = sql.firstRow(turnaroundQuery, sqlParams);
-		setTurnarounds(isBorrowing, getGroupDataMap(GROUP_ID_TOTAL, result), totalGroupTurnarounds)
+        profile("Running query for turnaroundQuery (total) (borrowing=${isBorrowing}, book=#{isBook}): " + turnaroundQuery + " params=" + sqlParams) {
+            def totalGroupTurnarounds = sql.firstRow(turnaroundQuery, sqlParams);
+            setTurnarounds(isBorrowing, getGroupDataMap(GROUP_ID_TOTAL, result), totalGroupTurnarounds)
+        }
 
 
-		log.debug("Running query for exhausted requests (borrowing=${isBorrowing}, book=#{isBook}): " + queryExhausted + " params="+sqlParams)
-		sql.eachRow(queryExhausted, sqlParams, {
-			int groupId = it.getAt(0) != null?it.getAt(0):GROUP_ID_TOTAL;
-			def groupData = getGroupDataMap(groupId, result)
-			groupData.exhaustedRequests = it.transNum
-			groupData.sumFees += it.sumFees
-		})
+        profile("Running query for exhausted requests (borrowing=${isBorrowing}, book=#{isBook}): " + queryExhausted + " params=" + sqlParams) {
+            sql.eachRow(queryExhausted, sqlParams, {
+                int groupId = it.getAt(0) != null ? it.getAt(0) : GROUP_ID_TOTAL;
+                def groupData = getGroupDataMap(groupId, result)
+                groupData.exhaustedRequests = it.transNum
+                groupData.sumFees += it.sumFees
+            })
+        }
 
-		log.debug(result);
-		return result
-	}
-	private void setTurnarounds(isBorrowing, groupData, srcRow){
-		if(isBorrowing){
-			groupData.turnaroundShpRec=srcRow.turnaroundShpRec
-			groupData.turnaroundReqShp=srcRow.turnaroundReqShp
-			groupData.turnaroundReqRec=srcRow.turnaroundReqRec
-		}else{
-			groupData.turnaround = srcRow.turnaround
-		}
-	}
-	private int getAdjustedGroupId(groupId, container){
-		if(groupId == GROUP_ID_TOTAL && ! container.contains(GROUP_ID_OTHER)){
-			return GROUP_ID_OTHER
-		}else{
-			return groupId;
-		}
-	}
-	private getGroupDataMap(groupId, container){
-		if(container.get(groupId) == null){
-			container.put(groupId, ['filledRequests':0, 'sumFees':0, 'exhaustedRequests':0]);
-		}
-		return container.get(groupId)
-	}
+        log.debug(result);
+        return result
+    }
 
-	private String getAdjustedQuery(query, stringMap){
+    private void setTurnarounds(isBorrowing, groupData, srcRow) {
+        if (isBorrowing) {
+            groupData.turnaroundShpRec = srcRow.turnaroundShpRec
+            groupData.turnaroundReqShp = srcRow.turnaroundReqShp
+            groupData.turnaroundReqRec = srcRow.turnaroundReqRec
+        } else {
+            groupData.turnaround = srcRow.turnaround
+        }
+    }
+
+    private int getAdjustedGroupId(groupId, container) {
+        if (groupId == GROUP_ID_TOTAL && !container.contains(GROUP_ID_OTHER)) {
+            return GROUP_ID_OTHER
+        } else {
+            return groupId;
+        }
+    }
+
+    private getGroupDataMap(groupId, container) {
+        if (container.get(groupId) == null) {
+            container.put(groupId, ['filledRequests': 0, 'sumFees': 0, 'exhaustedRequests': 0]);
+        }
+        return container.get(groupId)
+    }
+
+    private String getAdjustedQuery(query, stringMap) {
         String result = query;
-		stringMap.each() { key, value ->
-			result = result.replaceAll("\\{${key}\\}", value)
-		};
-		return result;
-	}
+        stringMap.each() { key, value ->
+            result = result.replaceAll("\\{${key}\\}", value)
+        };
+        return result;
+    }
 
-	def getGroupList(){
-		Sql sql = new Sql(dataSource_illiad);
-		return sql.rows(queries.lenderGroupList, [])
-	}
+    def getGroupList() {
+        Sql sql = new Sql(dataSource_illiad);
+        return sql.rows(queries.lenderGroupList, [])
+    }
+
+    def profile(String message, Closure closure) {
+        def start = new Date().time
+        closure.call()
+        def end = new Date().time
+        log.info "Profiling: [${message}] took ${end - start} ms"
+    }
 }
