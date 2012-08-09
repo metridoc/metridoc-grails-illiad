0b5cc674afdf18795fded1796866e59e80404efa
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-08-09 17:36:03 +0000

    removed a bug so that the _IlliadWorkflow is referenced in relationship to the plugin

--- scripts/RunIlliad.groovy
+++ scripts/RunIlliad.groovy
@@ -2,7 +2,7 @@ includeTargets << grailsScript("_GrailsBootstrap")
 
 target(main: "main entry point into the illiad script") {
     depends(compile, createConfig)
-    includeTargets << new File("scripts/_IlliadWorkflow.groovy")
+    includeTargets << new File("${metridocIlliadPluginDir}/scripts/_IlliadWorkflow.groovy")
     rootLoader.addURL(new File("${metridocIlliadPluginDir}/grails-app/conf").toURI().toURL())
 
     if(argsMap."do-bootstrap") {
