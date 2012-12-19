25cd9d2f02bbbdbbb284b00517a31874ce5513d5
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-12-19 15:13:05 +0000

    updated the application version to 0.2

--- MetridocIlliadGrailsPlugin.groovy
+++ MetridocIlliadGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import liquibase.integration.spring.SpringLiquibase
 
 class MetridocIlliadGrailsPlugin {
     // the plugin version
-    def version = "0.2-SNAPSHOT"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
