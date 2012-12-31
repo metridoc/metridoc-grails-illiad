806ff57f12ae0286987391027bed8ec31c427cbc
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-12-31 14:32:02 +0000

    upgraded the project to 2.2.0, upgaded the wrappers, since metridoc jobs are more restrictive when building camel registries, we added the dataSource property instead of relying on the application context, ignored some intellij files

--- grails-app/jobs/metridoc/illiad/IlliadJob.groovy
+++ grails-app/jobs/metridoc/illiad/IlliadJob.groovy
@@ -14,6 +14,7 @@ class IlliadJob extends MetridocJob {
     Sql _illiadDestinationSql
     def dataSource_from_illiad
     def dataSource_illiad
+    def dataSource
     def illiadService
     def grailsApplication
     def illiadWorkflowService
