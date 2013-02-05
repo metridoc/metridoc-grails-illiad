6e14463602ceaed67a32847c42a92bc50fe807f3
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2013-02-05 16:18:51 +0000

    got rid of the query-ui module dependency

--- grails-app/conf/IlliadResources.groovy
+++ grails-app/conf/IlliadResources.groovy
@@ -1,6 +1,5 @@
 modules = {
     illiad {
-        dependsOn 'jquery-ui'
         resource id: 'css', attrs: [type: 'css'],
                 url: [plugin: "metridocIlliad", dir:'illiad/css', file: 'illiad.css']
         resource id: 'js', attrs: [type: 'js'],
