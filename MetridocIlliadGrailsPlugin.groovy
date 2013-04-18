3bbdc0b58be2983960ffefcd196b4392e063215d
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2013-04-18 19:43:20 +0000

    Bumped the version to 0.3.2, cleaned out some classes and files that ar eno longer being used

--- MetridocIlliadGrailsPlugin.groovy
+++ MetridocIlliadGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class MetridocIlliadGrailsPlugin {
     // the plugin version
-    def version = "0.3.1"
+    def version = "0.3.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
