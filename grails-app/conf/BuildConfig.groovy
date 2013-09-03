67ae0f70d15a8bc5e2a93da2a356eec1a935e96f
Tommy Barker <mingus.karate@gmail.com>
2013-09-03 15:02:44 -0400

    add bintray upload plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -11,22 +11,20 @@ grails.project.dependency.resolution = {
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
         grailsCentral()
-        //TODO: get rid of this once we have locked down versions
-        mavenLocal()
         mavenCentral()
         mavenRepo "http://dl.bintray.com/upennlib/metridoc"
         mavenRepo "http://dl.bintray.com/upennlib/maven"
     }
 
     plugins {
-        //TODO: more up to date versions of the core do not require the exclusions.  remove once we update the core
         compile ':metridoc-core:0.7.2'
         runtime ':job-runner:0.6.1'
 
         build ':release:2.2.1',
                 ':rest-client-builder:1.0.3',
                 ":tomcat:$grailsVersion",
-                ':squeaky-clean:0.1.1', {
+                ':squeaky-clean:0.1.1',
+                ':bintray-upload:0.2', {
             export = false
         }
     }
