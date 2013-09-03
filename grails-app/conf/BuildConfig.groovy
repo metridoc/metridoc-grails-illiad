a655ae5dd9bd94f9b849d226f4c8e02f37a2e004
Tommy Barker <mingus.karate@gmail.com>
2013-09-03 14:58:09 -0400

    bump version to 0.3.5, buildConfig cleanup
    added bintray repo info so it will deploy as well

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,9 +2,8 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
-//location of the release repository
-grails.project.repos.metridocRepo.url = "svn:https://metridoc.googlecode.com/svn/maven/repository"
-//name of the repository
+grails.project.source.level = 1.6
+grails.project.repos.metridocRepo.url = "https://api.bintray.com/maven/upennlib/metridoc/metridoc-illiad"
 grails.project.repos.default = "metridocRepo"
 
 grails.project.dependency.resolution = {
@@ -21,12 +20,8 @@ grails.project.dependency.resolution = {
 
     plugins {
         //TODO: more up to date versions of the core do not require the exclusions.  remove once we update the core
-        compile(":metridoc-core:0.7.1") {
-            excludes "job-runner"
-            excludes "database-session"
-        }
-
-        runtime(":job-runner:0.6.1")
+        compile ':metridoc-core:0.7.2'
+        runtime ':job-runner:0.6.1'
 
         build ':release:2.2.1',
                 ':rest-client-builder:1.0.3',
